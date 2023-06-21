9a3eea2de0a72df2d3bd0c457e39050ca1d0a9c4
Gloria Pucoe <79689244+Gloria07-art@users.noreply.github.com>
2023-06-21 15:48:19 +0200

    Update Mydashboard.js

--- Mydashboard.js
+++ Mydashboard.js
@@ -1,31 +1,86 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import data from "./mock-data.json";
+import "./Mydashboard.css";
+import TodoForm from './TodoForm'
+import Todo from "./Todo";
+
+export function Mydashboard() {
+
+const navigate = useNavigate();
+
+const [stuffs, setStuffs] = useState([]);
+
+const addStuff = stuff => {
+    if(!stuff.text || /^\s*$/.test(stuff.text)) {
+        return;
+    }
+
+const newStuffs = [stuff, ...stuffs];
+
+setStuffs(newStuffs);
+};
 
 
-export const Mydashboard =()=> {
+const updatedStuff = (stuffId, newValue) => {
+    if(!newValue.text || /^\s*$/.test(newValue.text)) {
+        return; 
+}
+setStuffs(prev => prev.map(item => (item.id === stuffId ? newValue : item)));
+};
 
-const [stuff, setStuff] = useState(data);
 
-    const navigate = useNavigate();
+
+const removeStuff =id => {
+    const removeArr = [...stuffs].filter(stuff => stuff.id !==id);
+
+setStuffs(removeArr);
+};
+
+   
+const completeStuff = id => {
+    let updatedStuffs = stuffs.map(stuff => {
+        if (stuff.id === id) {
+            stuff.isComplete = !stuff.isComplete;
+        }
+    return stuff;
+});
+
+    setStuffs(updatedStuffs);
+};
     
 const [search,setSearch] =useState("");
 
-    return <div>
-<h1> To Do list</h1>
-<h3>"All life demands struggle. Those who have everything given to them become lazy, <br>
+
+
+
+
+
+ return( <div className="back">
+      
+        <div style={{backgroundColor:"AntiqueWhite ", width: "97%",  height: "1100px"}}>
+        <h1 class="top"> To Do list</h1>
+<h3 class="words">"All life demands struggle. Those who have everything given to them become lazy, <br>
 </br>selfish, and insensitive to the real values of life. The very striving and hard work<br></br>
  that we so constantly try to avoid is the major building block in the person we are today."</h3>
-<form>
-<input onChange = {(event => setSearch (event.target.value))} type="text" placeholder="Search"/>
-</form>
- <ul></ul>
-<div>
+<TodoForm onSubmit= {addStuff} />
+   
+<Todo
+stuffs={stuffs} 
+completeSuff ={completeStuff}
+removeStuff={removeStuff} 
+updatedStuff ={updatedStuff}
+/>
 
-    <li> <input type="text" placeholder="Write your task"/> <button>delete</button> <button>update</button></li>
-    <li> <input type="text" placeholder="Write your task"/> <button>delete</button> <button>update</button></li>
-    <button>Add</button>
+<button 
+type="submit" 
+class="out"
+onClick={()=>navigate(-1)}>log out</button>
+
 </div>
-<button type="submit" onClick={()=>navigate(-1)}>log out</button>
-    </div>
+        
+</div>
+
+   
+ );
 }
+
