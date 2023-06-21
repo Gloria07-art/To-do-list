4c17a00fa7b85ff8fd1f70e9d618edfcf8cec624
Gloria Pucoe <79689244+Gloria07-art@users.noreply.github.com>
2023-06-21 15:49:20 +0200

    Update EditOnlyForm.js

--- EditOnlyForm.js
+++ EditOnlyForm.js
@@ -9,24 +9,35 @@ const navigate = useNavigate();
         <div style={{backgroundColor:"AntiqueWhite ", width: "75%",  height: "1050px"}} >
 
         <div class="well" style={{width: "30%", height: "1050px" , float: "left"}}>
-        </div><ul></ul>
+        </div>
+        <ul></ul>
         
-         <h1>Register</h1> <ul></ul> 
+         <h1>Register</h1> 
+         <ul></ul> 
     <form>
     <h3>Name</h3>
-    <input type="text" placeholder=" Enter your Name"/> <ul></ul> 
+    <input type="text" required ="required" placeholder=" Enter your Name"/> <ul></ul> 
     <h3>Username</h3>
-    <input type="text" placeholder=" Enter your Username"/><ul></ul>
+    <input type="text"  required ="required" placeholder=" Enter your Username"/><ul></ul>
     <h3>Email</h3>
-    <input type="email" placeholder=" Enter your  Email"/><ul></ul>
+    <input type="email"  required ="required" placeholder=" Enter your  Email"/><ul></ul>
     <h3>Password</h3>
-    <input type="text" placeholder=" Enter your Password"/><ul></ul>
+    <input type="text"  required ="required" placeholder=" Enter your Password"/><ul></ul>
     <h3>Confirm Password</h3>
-    <input type="text" placeholder=" Please confirm"/>
-<h2><input class="box" type="checkbox"/> I fully agree with the applications terms and conditions </h2>
- </form><ul></ul>
- <button type="submit" onClick={()=>navigate("/readonly")}> Create Account</button>
-   <center> <h2>Powered by Mandelite</h2></center>
+    <input type="text"  required ="required" placeholder=" Please Confirm"/>
+
+<h2><input class="box" 
+ required ="required"
+style={{border:"none", borderRadius:"5px", width:"70px", height:"30px", backgroundColor:"white", color:"black", boxShadow:"4px 8px 16px black"}}
+type="checkbox"/> I fully agree with the applications terms and conditions. </h2>
+ </form>
+ <ul></ul>
+
+ <button
+ style={{boxShadow:"4px 8px 16px black"}}
+  type="submit" onClick={()=>navigate("/readonly")}> Create Account</button>
+
+   <center class="man"> <h2>**Powered by Mandelite**</h2></center>
     
     </div>
     </div> 
