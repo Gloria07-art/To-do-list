8ce5432c75541c7954e8bb4efc3ec71eae4415cc
Gloria Pucoe <79689244+Gloria07-art@users.noreply.github.com>
2023-06-21 15:50:26 +0200

    Update ReadOnly.js

--- ReadOnly.js
+++ ReadOnly.js
@@ -6,19 +6,23 @@ export const ReadOnly =()=>
  {
     const navigate = useNavigate();
     return <div  class="divide">
-        
-         <div style={{backgroundColor:"AntiqueWhite ", width: "30%",  height: "1050px"}} >
+        <div style={{backgroundColor:"AntiqueWhite ", width: "70%",  height: "1050px"}}>
+
+        <div class="well" style={{width: "30%", height: "1050px" , float: "left"}}>
+        </div>
         <h1>To Do List</h1>
-    <h2>User Login</h2>
+    <h2 className="log">User Login</h2>
     <form>
-        <input type="text" placeholder="Username"/> <ul></ul>
-        <input type="text" placeholder="Password"/>
+        <input type="text"  required ="required" placeholder="Username"/> <ul></ul>
+        <input type="text"  required ="required" placeholder="Password"/>
     </form> <ul></ul>
-    <button type="submit"  onClick={()=>navigate("/mydashboard")}>Login</button>
+    <button type="submit" 
+    style={{marginTop:"30px", marginBottom:"60px", boxShadow:"4px 8px 16px black"}}
+     onClick={()=>navigate("/mydashboard")}>Login</button>
 
     <h3> Forgot Username Password?</h3>
-    <h3> Create Your Account:</h3> <button onClick={()=>navigate(-1)}> Register </button>
-    <center> <h2>Powered by Mandelite</h2></center>
+    <h3> Create Your Account:</h3> <button style={{boxShadow:"4px 8px 16px black", backgroundColor:"aquamarine"}} onClick={()=>navigate(-1)}> Register </button>
+    <center class="mandel"> <h2>**Powered by Mandelite**</h2></center>
     </div>
     </div>
 }
